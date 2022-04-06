d5ca4186692fcf57f5b58c2fbf0028565b7228d9
olawale Ipadeola <ipadeolaolawale+github@gmail.com>
2022-04-06 14:56:31 +0200

    edge cases on input handling addressed

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: oipadeol <oipadeol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 19:02:23 by oipadeol          #+#    #+#             */
-/*   Updated: 2022/04/06 11:43:50 by oipadeol         ###   ########.fr       */
+/*   Updated: 2022/04/06 14:36:53 by oipadeol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,16 @@ typedef struct s_vars
 	void	*win;
 	void	*img;
 	char	start_orientation;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_lenght;
+	int		endian;
+	int		player[2];
 	char	**input;
 }t_vars;
 
-char	**input_rows(int argc, char **argv, char *start_orientation);
-int		check_valid(char **input, char *start_orientation);
+void	input_rows(int argc, char **argv, t_vars *vars);
+int		check_valid(char **input, t_vars *vars);
 void	ft_error(void);
 
 #endif
